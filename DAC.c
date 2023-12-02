f9c48e87accc513b0c9638653f9c936c6d3e9666
Nandini Kanawade <92260339+Nandini-Kanawade@users.noreply.github.com>
2023-12-02 19:26:02 +0530

    Update DAC.c

--- DAC.c
+++ DAC.c
@@ -1,136 +1,105 @@
-Dac initialization:
-void dac_init()
-	{
+//Dac initialization:
+void dac_init(){
      PINSEL1 |= 1<<19;
-	   DACR |= 1<< 16;
-	}
+     DACR |= 1<< 16;
+}
 
-Main File:
+
+//--------------------------------------------------
 #include <lpc214x.h>
 #include <stdio.h>
 #include "dac_init.c"
-
-void delay(unsigned int time)
-{	 
-		unsigned int j,i;
-		for( j=0;j<time;j++)
-		{
-			for(i=0;i<100;i++);
-		}
+void delay(unsigned int time){	 
+	unsigned int j,i;
+	for( j=0;j<time;j++){
+		for(i=0;i<100;i++);
+	}
 }
-
-int main()
-{
-		while(1)
-		{
-			DACR = 0xFFC0;
-			delay(2);
-			DACR = 0x000;
-			delay(2);	
+int main(){
+	while(1){
+		DACR = 0xFFC0;
+		delay(2);
+		DACR = 0x000;
+		delay(2);	
 	  }
 }
 
 
-Triangular Wave:
+//--------------------------------------------------
+//Triangular Wave:
 #include <lpc214x.h>
 #define DACPinMask 1<<19
 #define DAC_DataMask 0x0000FFC0
 #define DataShift 6
-
-void DAC_init()
-{
+void DAC_init(){
 	PINSEL1 |= (DACPinMask);
 	DACR |= 1<<16;
 }
-
-void delay(unsigned int time)
-{
+void delay(unsigned int time){
 	unsigned int i, j;
-	for(i=0;i<time;i++)
-	{
+	for(i=0;i<time;i++){
 		for(j=0; j<1000; j++);
 	}
 }
-
-int main(void)
-{
+int main(void){
 	DAC_init();
-	while(1)
-	{
-		for(int i=0; i<=1023 ; i++)
-		{
+	while(1){
+		for(int i=0; i<=1023 ; i++){
 			DACR=(1<<16 | i<<DataShift);
 		}
-		
-		for(int i=1022; i>=1 ; i--)
-		{
-			DACR=(1<<16 | i<<DataShift);
-		}
-	}
-}
-
-Sawtooth Wave:
-#include <lpc214x.h>
-#define DACPinMask 1<<19
-#define DAC_DataMask 0x0000FFC0
-#define DataShift 6
-
-void DAC_init()
-{
-	PINSEL1 |= (DACPinMask);
-	DACR |= 1<<16;
-}
-
-void delay(unsigned int time)
-{
-	unsigned int i, j;
-	for(i=0;i<time;i++)
-	{
-		for(j=0; j<1000; j++);
-	}
-}
-
-int main(void)
-{
-	DAC_init();
-	while(1)
-	{
-		for(int i=1023; i>=1 ; i--)
-		{
+		for(int i=1022; i>=1 ; i--){
 			DACR=(1<<16 | i<<DataShift);
 		}
 	}
 }
 
 
-Staircase Wave:
+//--------------------------------------------------
+//Sawtooth Wave:
 #include <lpc214x.h>
 #define DACPinMask 1<<19
 #define DAC_DataMask 0x0000FFC0
 #define DataShift 6
-
-void DAC_init()
-{
+void DAC_init(){
 	PINSEL1 |= (DACPinMask);
 	DACR |= 1<<16;
 }
-
-void delay(unsigned int time)
-{
+void delay(unsigned int time){
 	unsigned int i, j;
-	for(i=0;i<time;i++)
-	{
+	for(i=0;i<time;i++){
 		for(j=0; j<1000; j++);
 	}
 }
-
-int main(void)
-{
+int main(void){
 	DAC_init();
-	while(1)
-	{
-		for(int i=0; i<=1023 ; i += 200)
-		{
+	while(1){
+		for(int i=1023; i>=1 ; i--){
+			DACR=(1<<16 | i<<DataShift);
+		}
+	}
+}
+
+
+//--------------------------------------------------
+//Staircase Wave:
+#include <lpc214x.h>
+#define DACPinMask 1<<19
+#define DAC_DataMask 0x0000FFC0
+#define DataShift 6
+void DAC_init(){
+	PINSEL1 |= (DACPinMask);
+	DACR |= 1<<16;
+}
+void delay(unsigned int time){
+	unsigned int i, j;
+	for(i=0;i<time;i++){
+		for(j=0; j<1000; j++);
+	}
+}
+int main(void){
+	DAC_init();
+	while(1){
+		for(int i=0; i<=1023 ; i += 200){
 			DACR=(1<<16 | i<<DataShift);
 		}
 	}
